4a12faae2288fbf56c422aaa107455f4201aef1a
Paulo Victor <paulovictordev16@gmail.com>
2026-04-21 20:04:56 -0300

    feat: add constants for quick buttons content

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const quickButtonsContent = [
+	{
+		label: "Nature 🌳",
+		value: "Nature",
+	},
+	{
+		label: "Car 🚘",
+		value: "Car",
+	},
+	{
+		label: "Animals 🦄",
+		value: "Animals",
+	},
+	{
+		label: "Buildings 🏢",
+		value: "Buildings",
+	},
+	{
+		label: "Sports ⚽",
+		value: "Sports",
+	},
+	{
+		label: "People 👥",
+		value: "People",
+	},
+];
