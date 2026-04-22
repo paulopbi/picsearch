c01be365b09385ce949f6a98d6a8876c17f62b8a
Paulo Victor <paulovictordev16@gmail.com>
2026-04-22 20:12:37 -0300

    feat: add Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import { Aperture } from "lucide-react";
+import "./Logo.css";
+
+const Logo = () => {
+	return (
+		<h6 className="logo">
+			Flashdash <Aperture />
+		</h6>
+	);
+};
+
+export default Logo;
